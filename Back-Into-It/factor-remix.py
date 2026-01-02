68bda48e56614bc8a29b3ac231e8561f0f602295
Hex <hexasweetzmain@gmail.com>
2026-01-01 21:30:16 -0500

    Update factor-remix.py

--- Back-Into-It/factor-remix.py
+++ Back-Into-It/factor-remix.py
@@ -1,3 +1,10 @@
+
+#idea - allow any number but have an if statement to check to see if its between 0-22
+
+#.. then if it is return factorial, if not return invalid error
+
+import math 
+
 print("Enter a number: ")
 
 num = int(input(">"))
