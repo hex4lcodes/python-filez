59480e3f95a481eda61a39270456fc325eab4b74
Hex <hexasweetzmain@gmail.com>
2026-01-01 23:23:16 -0500

    Create calculator-kinda.py
    I swear this took way too long!

---
+++ Back-Into-It/calculator-kinda.py
@@ -0,0 +1,58 @@
+#this is gonna be a pretty basic calculator (big 4 operations (+,-,*,/) and radians and degrees)
+#the values are stored as floats to account for values used in radians and degree calculations
+
+#exit stops the program if a block evaluates to true
+
+#fixed the or statement to more accurately compute each block
+
+import math
+
+print("This calcualtor allows for additon, subraction, multiplication, division, radians and degrees")
+print("If you have only one number to work with fill in 0 for the second value")
+
+print("\n")
+
+print("Enter the first number you will be working with")
+val1 = float(input(">"))
+
+print("Enter the second number you will be working with")
+val2 = float(input(">"))
+
+print("\n")
+
+print("What calculation would you like to do: a addition, s subtraction, m multiplication, g for division")
+print("You can use D for degrees and R for radians")
+operation = input(">")
+
+if operation == 'a' or operation == 'A':
+    print(val1 + val2)
+elif operation == 's' or operation == 'S':
+    print(val1 - val2)
+elif operation == 'm' or operation == 'M':
+    print(val1 * val2)
+elif operation == 'g' or operation == 'G':
+    print(val1/val2)  
+else:
+    pass
+
+exit()
+
+
+
+if operation == 'd' or operation == 'D':
+    print(math.degrees(val1))
+    exit()
+    if  val2 != 0:
+        print(math.degrees(val2))
+    else:
+        pass
+elif operation == 'r' or operation == 'R':
+    print(math.radians(val1))
+    exit()
+
+    if  val2 != 0:
+        print(math.radians(val2))
+    else:
+        pass
+else:
+    print("Something went wrong, try again")
