7c0cd3038e812dc322b10e1787c2c92f65c3f2fd
Hex <hexasweetzmain@gmail.com>
2026-01-05 21:56:36 -0500

    Create password.py

---
+++ Basic-Concepts/password.py
@@ -0,0 +1,36 @@
+"""
+Password checker
+
+Thank you to Reddit user LebronJayce for what became the shell for this project!!
+And thanks to the wonderful folks at GeeksforGeeks for the explanations!!
+
+Password must have: 8 characters, both upper and lowercase letters, at least one number
+"""
+
+print("Enter a password below (plese ommitt special charaters!): ")
+
+password = input('> ')
+
+if len(password) < 8:
+    print("Password too short, try again!")
+    exit()
+else:
+    pass
+
+if password.isalnum() and not password.isdigit() and not password.isalpha(): #will test to see if password is alphanumeric - not just letter / number only
+    print("Strong password!")
+    exit()
+else:
+    pass
+
+if password.isdigit():
+    print("Password cannot contain only numbers, password must contain letters and numbers")
+    exit()
+else:
+    pass
+
+if password.isalpha():
+    print("Password cannot contain only letters, password must contain letters and numbers")
+    exit()
+else:
+    pass
