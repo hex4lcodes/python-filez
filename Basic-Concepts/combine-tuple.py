0f245d850d2a1a69c7ba4e4c6359554e9e04f77a
Hex <hexasweetzmain@gmail.com>
2026-01-07 21:40:06 -0500

    Create combine-tuple.py

---
+++ Basic-Concepts/combine-tuple.py
@@ -0,0 +1,14 @@
+"""
+Steps:
+    (1) make two tuples
+    (2) combine them into one tuple
+"""
+
+nset1 = (1, 2, 3)
+nset2 = (4, 5, 6)
+
+combined_nset = nset1 + nset2 #concatenate tuples like you would combine numbers / strings
+
+print(nset1)
+print(nset2)
+print(combined_nset)
