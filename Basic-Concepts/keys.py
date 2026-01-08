b0b91e4f8b7786d149d25d92f3043aaf8c961b5f
Hex <hexasweetzmain@gmail.com>
2026-01-08 01:39:12 -0500

    Create keys.py

---
+++ Basic-Concepts/keys.py
@@ -0,0 +1,6 @@
+#loop through values and keys
+
+info = {"name": "Jane", "age": "48", "favefood": "icecream"} #dictionary structure: "key": "value"
+
+for key, value in info.items():
+    print("{}: {}" .format(key, value))
