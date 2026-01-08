df1b0ad8fb8b2d24fbee0d988b2966c4cd33af1c
Hex <hexasweetzmain@gmail.com>
2026-01-07 22:02:12 -0500

    Create nestedtuple.py

---
+++ Basic-Concepts/nestedtuple.py
@@ -0,0 +1,13 @@
+"""
+Instructions:
+     (1)  Create a nested tuple
+     (2) Print the 2nd element of the 1st nested tuple
+"""
+
+nested_tuple = ((1, 2), (3, 4), (5, 6)) #tuples combine in order, ex. tuple1 = 1,2,3 then tuple2 = 4,5,6
+
+#the second element of tuple 1 would be @ index 0 and sub index 1, all collections start @ index 0 - even parts
+
+print(nested_tuple)
+print(nested_tuple[0])
+print(nested_tuple[0][1])
