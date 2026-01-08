64a9d0a6d06e112911e7c3109466c4975edb3577
Hex <hexasweetzmain@gmail.com>
2026-01-08 15:11:43 -0500

    Create liftoff.py

---
+++ Basic-Concepts/liftoff.py
@@ -0,0 +1,13 @@
+#use a while loop to print a counter from 10-1 and print "Liftoff!" after its done
+
+
+countdown = 10
+
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+
+    if countdown == 0:
+        print("Liftoff!")
+    else:
+        pass
